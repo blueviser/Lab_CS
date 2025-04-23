877e36167be5561a71ab90f13538bca2eba49239
blueviser <tatyanashitina@gmail.com>
2025-04-23 10:41:33 +0800

    WebAPI

---
+++ ВебАПИ/WebAPI/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public int Duration { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ ВебАПИ/WebAPI/CourseContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI;
+
+public class CourseContext : DbContext
+{
+    public CourseContext(DbContextOptions<CourseContext> options) : base(options) { }
+
+    public DbSet<Course> Courses { get; set; }
+}
